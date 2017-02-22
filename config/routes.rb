7618923fe3bd9fcf4df76571a945a993e45be972
Justinio14 <jbutwell@hotmail.com>
2017-02-22 12:50:16 +0000

    Section 2 ex1 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :restaurants do
     resources :reviews
     end
-  get 'restaurants' => 'restaurants#index'
+  root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
